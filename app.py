9ed2382c65a536a649792416d4c8ae7fa199ebf3
Kevin Templeton <templeton.kevin@gmail.com>
2022-07-26 13:24:39 -0500

    step 7 complete

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "oh-so-secret"
+app.config['SECRET_KEY'] = 'oh-so-secret'
 
 debug = DebugToolbarExtension(app)
 
@@ -20,19 +20,33 @@ def question(qid):
     """Show a form asking the current question, and listing the choices as radio buttons.
     Answering the question should fire off a POST request to /answer."""
 
-    question = satisfaction_survey.questions[qid]
+    if len(responses) == len(satisfaction_survey.questions):
+        flash("You tried to access a question again, but the survey is complete.")
+        return redirect('/complete')
     
-    return render_template('question.html', question=question, question_num=qid)
+    elif qid == len(responses):
+        question = satisfaction_survey.questions[qid]
+        return render_template('question.html', question=question, question_num=qid)
+    
+    else:
+        flash("You tried to access an invalid question. This is the correct one.")
+        return redirect(f'/questions/{len(responses)}')
 
-@app.route('/answer', methods=["POST"])
+@app.route('/answer', methods=['POST'])
 def answer():
     """POST request--append the user's answer to our responses list, then redirect to next question."""
 
-    response = request.form["answer"]
+    response = request.form['answer']
     responses.append(response)
     
     if (len(responses) == len(satisfaction_survey.questions)):
-        return redirect("/complete")
+        return redirect('/complete')
 
     else:
-        return redirect(f"/questions/{len(responses)}")
+        return redirect(f'/questions/{len(responses)}')
+
+@app.route('/complete')
+def thank_you():
+    """Thank the user for completing the survey."""
+
+    return render_template('complete.html', survey=satisfaction_survey)
