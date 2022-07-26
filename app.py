cc9c042aea54ab822612d1155444810f84f58386
Kevin Templeton <templeton.kevin@gmail.com>
2022-07-26 08:40:05 -0500

    question and answer routes complete

--- app.py
+++ app.py
@@ -15,8 +15,24 @@ def homepage():
 
     return render_template('homepage.html', survey=satisfaction_survey)
 
-@app.route('/questions/<num>')
-def homepage():
-    """Show the title of the survey, the instructions, and a button to start the survey."""
+@app.route('/questions/<int:num>')
+def question():
+    """Show a form asking the current question, and listing the choices as radio buttons.
+    Answering the question should fire off a POST request to /answer."""
 
-    return render_template('homepage.html', survey=satisfaction_survey)
+    question=satisfaction_survey.questions[num]
+    
+    return render_template('question.html', question=question, num=num)
+
+@app.route('/answer', methods=["POST"])
+def answer():
+    """POST request--append the user's answer to our responses list, then redirect to next question."""
+
+    response = request.form["answer"]
+    responses.append(response)
+    
+    if (len(responses) == len(satisfaction_survey.questions)):
+        return redirect("/complete")
+
+    else:
+        return redirect(f"/questions/{len(responses)}")
