2da0e8b6a281ddaee0c34d1499c8ccfe74380bfe
Kevin Templeton <templeton.kevin@gmail.com>
2022-07-29 08:08:34 -0500

    step 8 complete

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'oh-so-secret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
-
-responses = []
 
 @app.route('/')
 def homepage():
@@ -15,35 +14,46 @@ def homepage():
 
     return render_template('homepage.html', survey=satisfaction_survey)
 
+@app.route('/start-session', methods=['POST'])
+def start_session():
+    """Set session['responses'] to an empty list, then redirect to the first question."""
+
+    session['responses'] = []
+
+    return redirect('questions/0')
+
 @app.route('/questions/<int:qid>')
 def question(qid):
     """Show a form asking the current question, and listing the choices as radio buttons.
     Answering the question should fire off a POST request to /answer."""
 
-    if len(responses) == len(satisfaction_survey.questions):
+    if len(session['responses']) == len(satisfaction_survey.questions):
         flash("You tried to access a question again, but the survey is complete.")
         return redirect('/complete')
     
-    elif qid == len(responses):
+    elif qid == len(session['responses']):
         question = satisfaction_survey.questions[qid]
         return render_template('question.html', question=question, question_num=qid)
     
     else:
         flash("You tried to access an invalid question. This is the correct one.")
-        return redirect(f'/questions/{len(responses)}')
+        return redirect(f'/questions/{len(session["responses"])}')
 
 @app.route('/answer', methods=['POST'])
 def answer():
-    """POST request--append the user's answer to our responses list, then redirect to next question."""
+    """POST request--append the user's answer to the session responses list,
+    then redirect to next question."""
 
     response = request.form['answer']
+    responses = session['responses']
     responses.append(response)
+    session['responses'] = responses
     
-    if (len(responses) == len(satisfaction_survey.questions)):
+    if (len(session['responses']) == len(satisfaction_survey.questions)):
         return redirect('/complete')
 
     else:
-        return redirect(f'/questions/{len(responses)}')
+        return redirect(f'/questions/{len(session["responses"])}')
 
 @app.route('/complete')
 def thank_you():
